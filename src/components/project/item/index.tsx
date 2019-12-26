986729c3d073a4d361414f43452fb6f0b1ad6f70
João Dias <joao.dias@feedzai.com>
2019-12-26 15:38:14 +0000

    - removed deprecated prop

--- src/components/project/item/index.tsx
+++ src/components/project/item/index.tsx
@@ -34,7 +34,6 @@ export const PortfolioItem: FunctionComponent<IPortfolioItemProps> = ({
 						position: "absolute",
 					}}
 					backgroundColor={color}
-					critical={false}
 					loading="lazy"
 					imgStyle={{
 						position: "relative",
