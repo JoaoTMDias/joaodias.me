2378690372d9f5871632cfa0091e0d86136483c6
João Dias <joao.dias@feedzai.com>
2020-05-06 14:50:29 +0100

    Added testes for input validation

--- cypress/integration/contact.js
+++ cypress/integration/contact.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+// / <reference types="cypress" />
 import { setupLayout } from "../mocks/setupLayout";
 
 const PAGE_URL = `${Cypress.config().baseUrl}/contact`;
@@ -6,8 +6,29 @@ const PAGE_URL = `${Cypress.config().baseUrl}/contact`;
 const EXPECTED = {
 	name: "John Doe",
 	email: "email@johndoe.com",
-	message: "This is a debugging message"
+	message: "This is a debugging message",
+	invalid: {
+		name: {
+			empty: "A name is required",
+			min: "Minimum of 3 characters",
+			max: "The maximum is 50 characters",
+		},
+		email: {
+			empty: "An email is required",
+			invalid: "That's not really an email, is it?",
+			min: "Minimum of 3 characters",
+			max: "The maximum is 50 characters",
+		},
+		message: {
+			empty: "A message is required",
+			min: "Minimum of 10 characters",
+			max: "The maximum is 500. Try typing a shorter message.",
+		},
+	},
 };
+
+const LONG_MESSAGE =
+	"fhxkzxjwqtxrsyexosjihrezsytescpzqhwwqhgvvdyjsphuhdmabhazfdltmmlfwegfznaghleqasfugtrybmfxtmuppokhuzadbonkeliehcssyxzobnngpvmvqegvgzctcfrkoogcdhfzgnaxbzftbusyskrlnmdyapjbzqvlmhanwjysopptwbenikxdhnxfefbxtadgqgxxetoycehczinozonblhltyorhptzyyggxmyzvnonfuplshenaazahfdulkltkianrnkhorwcykduzepdcqprjmiupfmvhmgjbdyfgjjnoqxeczevidtbphleerxhpuagnlxmejezpzivrxcxdznuegwajjwjymwzppvrxtpxdvmhmiodvvokupwlgellrgqeswufhlumehnezcocarywrwpwcdusaxmhqetbkvfqaqsiberpdjxetqdxuupuglaoayqrquwieeedmeoxprdompghxhpmysiqsshvyfylzblveavdi";
 
 describe("Contact", () => {
 	beforeEach(() => {
@@ -17,10 +38,10 @@ describe("Contact", () => {
 			url: "/",
 			body: {
 				"form-name": "contact-form",
-				"name": EXPECTED.name,
-				"email": EXPECTED.email,
-				"message": EXPECTED.message,
-				"wasSent": true
+				name: EXPECTED.name,
+				email: EXPECTED.email,
+				message: EXPECTED.message,
+				wasSent: true,
 			},
 		}).as("formSubmit");
 	});
@@ -50,86 +71,86 @@ describe("Contact", () => {
 			});
 
 			describe("should stay disabled when some fields are left empty or invalid", () => {
-				it("empty: name", () => {
-					cy.fillForm([
-						{
-							alias: "@emailInput",
-							value: EXPECTED.email
-						},
-						{
-							alias: "@messageTextarea",
-							value: {
-								text: EXPECTED.message,
-								options: {
-									force: true
-								}
-							}
-						},
-					]);
+				describe("when name is...", () => {
+					it("empty", () => {
+						cy.fillForm([
+							{
+								alias: "@emailInput",
+								value: EXPECTED.email,
+							},
+							{
+								alias: "@messageTextarea",
+								value: {
+									text: EXPECTED.message,
+									options: {
+										force: true,
+									},
+								},
+							},
+						]);
 
-					cy.get("@submitButton")
-						.focus()
-						.invoke("attr", "aria-disabled")
-						.should("be", true);
+						cy.get("@submitButton").focus().invoke("attr", "aria-disabled").should("be", true);
+					});
 				});
 
-				it("empty: email", () => {
-					cy.fillForm([
-						{
-							alias: "@nameInput",
-							value: EXPECTED.name
-						},
-						{
-							alias: "@messageTextarea",
-							value: {
-								text: EXPECTED.message,
-								options: {
-									force: true
-								}
-							}
-						},
-					]);
-					cy.get("@submitButton")
-						.focus().invoke("attr", "aria-disabled").should("be", true);
+				describe("when email is...", () => {
+					it("empty", () => {
+						cy.fillForm([
+							{
+								alias: "@nameInput",
+								value: EXPECTED.name,
+							},
+							{
+								alias: "@messageTextarea",
+								value: {
+									text: EXPECTED.message,
+									options: {
+										force: true,
+									},
+								},
+							},
+						]);
+						cy.get("@submitButton").focus().invoke("attr", "aria-disabled").should("be", true);
+					});
+
+					it("invalid", () => {
+						cy.fillForm([
+							{
+								alias: "@nameInput",
+								value: EXPECTED.name,
+							},
+							{
+								alias: "@emailInput",
+								value: "emailatjohndoe.com",
+							},
+							{
+								alias: "@messageTextarea",
+								value: {
+									text: EXPECTED.message,
+									options: {
+										force: true,
+									},
+								},
+							},
+						]);
+						cy.get("@submitButton").focus().invoke("attr", "aria-disabled").should("be", true);
+					});
 				});
 
-				it("invalid: email", () => {
-					cy.fillForm([
-						{
-							alias: "@nameInput",
-							value: EXPECTED.name
-						},
-						{
-							alias: "@emailInput",
-							value: "emailatjohndoe.com"
-						},
-						{
-							alias: "@messageTextarea",
-							value: {
-								text: EXPECTED.message,
-								options: {
-									force: true
-								}
-							}
-						},
-					]);
-					cy.get("@submitButton")
-						.focus().invoke("attr", "aria-disabled").should("be", true);
-				});
-
-				it("empty: message", () => {
-					cy.fillForm([
-						{
-							alias: "@nameInput",
-							value: EXPECTED.name
-						},
-						{
-							alias: "@emailInput",
-							value: EXPECTED.email
-						},
-					]);
-					cy.get("@submitButton")
-						.focus().invoke("attr", "aria-disabled").should("be", true);
+				describe("when message is...", () => {
+					it("empty", () => {
+						cy.fillForm([
+							{
+								alias: "@nameInput",
+								value: EXPECTED.name,
+							},
+							{
+								alias: "@emailInput",
+								value: EXPECTED.email,
+							},
+						]);
+						cy.get("@submitButton").focus().invoke("attr", "aria-disabled").should("be", true);
+					});
 				});
 			});
 
@@ -137,27 +158,124 @@ describe("Contact", () => {
 				cy.fillForm([
 					{
 						alias: "@nameInput",
-						value: EXPECTED.name
+						value: EXPECTED.name,
 					},
 					{
 						alias: "@emailInput",
-						value: EXPECTED.email
+						value: EXPECTED.email,
 					},
 					{
 						alias: "@messageTextarea",
 						value: {
 							text: EXPECTED.message,
 							options: {
-								force: true
-							}
-						}
+								force: true,
+							},
+						},
 					},
 				]);
 
-				cy.get("@submitButton")
-					.focus()
-					.invoke("attr", "aria-disabled")
-					.should("be", false);
+				cy.get("@submitButton").focus().invoke("attr", "aria-disabled").should("be", false);
+			});
+		});
+
+		describe("inline validation", () => {
+			it("should not show any errors by default", () => {
+				cy.get("@nameInputWrapper").should("not.have.class", "has-errors");
+				cy.get("@emailInputWrapper").should("not.have.class", "has-errors");
+				cy.get("@messageTextareaWrapper").should("not.have.class", "has-errors");
+			});
+
+			describe("should show an error", () => {
+				describe("when name...", () => {
+					it("is empty", () => {
+						cy.get("@nameInput").focus().blur();
+						cy.get("@nameInputWrapper").find(".helper").contains(EXPECTED.invalid.name.empty);
+					});
+
+					it("has less than the minimum", () => {
+						cy.get("@nameInput").focus().type("ab").blur();
+						cy.get("@nameInputWrapper").find(".helper").contains(EXPECTED.invalid.name.min);
+					});
+
+					describe("has more than the maximum", () => {
+						it("natively limited by the browser", () => {
+							cy.get("@nameInput").focus().type("TL7K9xAEa7D_z98N]H{S.-g2d[jAUv%3Wqf:vJ4w^#e$e'u@`77").blur();
+							cy.get("@nameInputWrapper").find(".helper").should("not.exist");
+						});
+
+						it("limited by javascript", () => {
+							cy.get("@nameInput").then((element) => {
+								element.attr("maxLength", "");
+							});
+							cy.get("@nameInput").focus().type("TL7K9xAEa7D_z98N]H{S.-g2d[jAUv%3Wqf:vJ4w^#e$e'u@`77").blur();
+							cy.get("@nameInputWrapper").find(".helper").contains(EXPECTED.invalid.name.max);
+						});
+					});
+				});
+
+				describe("when email...", () => {
+					it("is empty", () => {
+						cy.get("@emailInput").focus().blur();
+						cy.get("@emailInputWrapper").find(".helper").contains(EXPECTED.invalid.email.empty);
+					});
+
+					it("is not valid", () => {
+						cy.get("@emailInput").focus().type("email-at-email.com").blur();
+						cy.get("@emailInputWrapper").find(".helper").contains(EXPECTED.invalid.email.invalid);
+					});
+
+					describe("has more than the maximum", () => {
+						it("natively limited by the browser", () => {
+							cy.get("@emailInput")
+								.focus()
+								.type("nikxeizpcltkvhxeqnwbauiwtqtmhhjppxhofwvjoidqpbkpyjoidqpbkpy@gmail.com")
+								.blur();
+							cy.get("@emailInputWrapper").find(".helper").contains(EXPECTED.invalid.email.invalid);
+							cy.get("@emailInputWrapper").find(".helper").should("not.contain.text", EXPECTED.invalid.email.max);
+						});
+
+						it("limited by javascript", () => {
+							cy.get("@emailInput").then((element) => {
+								element.attr("maxLength", "");
+							});
+							cy.get("@emailInput")
+								.focus()
+								.type("nikxeizpcltkvhxeqnwbauiwtqtmhhjppxhofwvjoidqpbkpyjoidqpbkpy@gmail.com")
+								.blur();
+							cy.get("@emailInputWrapper").find(".helper").contains(EXPECTED.invalid.email.max);
+						});
+					});
+				});
+
+				describe("when message...", () => {
+					it("is empty", () => {
+						cy.get("@messageTextarea").focus().blur();
+						cy.get("@messageTextareaWrapper").find(".helper").contains(EXPECTED.invalid.message.empty);
+					});
+
+					it("has less than the minimum", () => {
+						cy.get("@messageTextarea").focus().type("ab").blur();
+						cy.get("@messageTextareaWrapper").find(".helper").contains(EXPECTED.invalid.message.min);
+					});
+
+					describe("has more than the maximum", () => {
+						it("natively limited by the browser", () => {
+							cy.get("@messageTextarea").focus().type(LONG_MESSAGE).blur();
+							cy.get("@messageTextareaWrapper")
+								.find(".helper")
+								.should("not.contain.text", EXPECTED.invalid.message.max);
+						});
+
+						it("limited by javascript", () => {
+							cy.get("@messageTextarea").then((element) => {
+								element.attr("maxLength", "");
+							});
+							cy.get("@messageTextarea").focus().type(LONG_MESSAGE).blur();
+							cy.get("@messageTextareaWrapper").find(".helper").contains(EXPECTED.invalid.message.max);
+						});
+					});
+				});
 			});
 		});
 	});
--- src/components/forms/validation-schema.ts
+++ src/components/forms/validation-schema.ts
@@ -1,28 +1,32 @@
-import { object, string } from 'yup';
+import { object, string } from "yup";
 
 export const FIELDS_BOUNDARIES = {
-	input: {
+	name: {
 		min: 3,
-		max: 50
+		max: 50,
+	},
+	email: {
+		min: 5,
+		max: 50,
 	},
 	textarea: {
 		min: 10,
-		max: 500
-	}
-}
+		max: 500,
+	},
+};
 
 export const CONTACT_FORM_SCHEMA = object().shape({
 	name: string()
-		.min(FIELDS_BOUNDARIES.input.min, `Min of ${FIELDS_BOUNDARIES.input.min} characters`)
-		.max(FIELDS_BOUNDARIES.input.max, 'Woa, hold there, champ. The max is 50 characters')
-		.required('Name is required'),
+		.min(FIELDS_BOUNDARIES.name.min, `Minimum of ${FIELDS_BOUNDARIES.name.min} characters`)
+		.max(FIELDS_BOUNDARIES.name.max, "The maximum is 50 characters")
+		.required("A name is required"),
 	email: string()
 		.email(`That's not really an email, is it?`)
-		.min(FIELDS_BOUNDARIES.input.min, `Min of ${FIELDS_BOUNDARIES.input.min} characters`)
-		.max(FIELDS_BOUNDARIES.input.max, `A long-ass email. The max is 50 ok?`)
-		.required('Email is required'),
+		.min(FIELDS_BOUNDARIES.email.min, `Minimum of ${FIELDS_BOUNDARIES.email.min} characters`)
+		.max(FIELDS_BOUNDARIES.email.max, `The maximum is 50 characters`)
+		.required("An email is required"),
 	message: string()
-		.min(FIELDS_BOUNDARIES.textarea.min, `Min of ${FIELDS_BOUNDARIES.textarea.min} characters`)
-		.max(FIELDS_BOUNDARIES.textarea.max, 'Try typing a shorter message. The maximum characters is 500')
-		.required('A message is required'),
+		.min(FIELDS_BOUNDARIES.textarea.min, `Minimum of ${FIELDS_BOUNDARIES.textarea.min} characters`)
+		.max(FIELDS_BOUNDARIES.textarea.max, "The maximum is 500. Try typing a shorter message.")
+		.required("A message is required"),
 });
