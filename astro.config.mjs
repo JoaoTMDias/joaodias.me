2f37fc4f6c672d5a6bdd5cc79f66c6fa16af1763
Joao Dias <joao.dias@feedzai.com>
2022-09-15 10:39:06 +0300

    feat(Web Components): Added lit and @astrojs/lit
    Closes #14

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,11 @@
  */
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import lit from "@astrojs/lit";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    lit()
+],
 });
