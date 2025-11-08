ca2db8d3a08a14a1c4b28bcb392256afa5e613ed
João Dias <joao.dias@feedzai.com>
2025-11-08 19:17:24 +0000

    chore: remove unused content types and clean up CSS styles
    - Deleted the content-types and selected-work type definitions to streamline the codebase.
    - Removed empty CSS rules from the intro component for improved clarity and maintainability.
    - Updated the services configuration to export only the necessary LAST_FM_URL variable.

--- src/data/services/config.ts
+++ src/data/services/config.ts
@@ -12,4 +12,4 @@ const LAST_FM_USERNAME = "jtmdias";
 const LAST_FM_API_KEY = import.meta.env.PUBLIC_LAST_FM_API_KEY;
 const LAST_FM_URL = `https://ws.audioscrobbler.com/2.0/?method=${LAST_FM_SEARCH_METHOD}&user=${LAST_FM_USERNAME}&api_key=${LAST_FM_API_KEY}&format=json`;
 
-export { LAST_FM_SEARCH_METHOD, LAST_FM_USERNAME, LAST_FM_API_KEY, LAST_FM_URL };
+export { LAST_FM_URL };
