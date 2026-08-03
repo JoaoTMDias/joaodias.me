49768fed8110d877ecf3dc5e2d8b07ade92c40ca
João Dias <contacto@joaodias.me>
2026-08-03 10:12:25 +0100

    feat: implement jd-accordion component

---
+++ src/components/jd-accordion/index.ts
@@ -0,0 +1,5 @@
+import "./jd-accordion-item.js";
+import "./jd-accordion.js";
+
+export { JdAccordion } from "./jd-accordion.js";
+export { JdAccordionItem } from "./jd-accordion-item.js";
--- src/env.d.ts
+++ src/env.d.ts
@@ -7,3 +7,23 @@ interface ImportMetaEnv {
 interface ImportMeta {
 	readonly env: ImportMetaEnv;
 }
+
+declare namespace astroHTML.JSX {
+	interface IntrinsicElements {
+		'jd-accordion': {
+			'allow-multiple'?: boolean | string;
+			class?: string;
+			id?: string;
+			children?: any;
+		};
+		'jd-accordion-item': {
+			category?: string;
+			label: string;
+			open?: boolean | string;
+			'heading-level'?: number | string;
+			class?: string;
+			id?: string;
+			children?: any;
+		};
+	}
+}
